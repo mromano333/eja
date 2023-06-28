729f920474c188529818bdc048a6b30ae0865859
mromano333 <40279201+mromano333@users.noreply.github.com>
2023-06-28 16:41:32 -0400

    Update example.spec.ts

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,19 +1,18 @@
 import { test, expect } from '@playwright/test';
 
-test('Visit Phoenix dev site and verify the title and login', async ({ page }) => {
+test('Visit Phoenix dev site and verify the title, quote, and login', async ({ page }) => {
   await page.goto('https://thrive.thrivetrm.cloud/');
 
   await expect(page).toHaveTitle(/NxGen Front-End/);
 
-  await expect(page).toHaveText(/Let's build relationships, not just pipelines./);
+  const quote = await page.getByText(/Let's build relationships, not just pipelines./);
+  await expect(quote !== undefined ).toBeTruthy();
 
   await page.locator('input[name="username"]').click();
   await page.locator('input[name="username"]').fill('support@thrivetrm.com');
 
   await page.locator('input[name="password"]').click();
   await page.locator('input[name="password"]').fill('password');
-
-  await page.locator('[placeholder="Password"]').press('Enter');
 
   await page.locator('input[type="checkbox"]').check();
 
